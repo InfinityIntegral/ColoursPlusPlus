95d203e67d360041d90d6e4cc1338fb3ef1537e8
Infinity Integral (05524F) <infinityintegral333@gmail.com>
2025-12-03 07:17:32 +0800

    done progress bar

--- include/SGCLPDisplayPage.h
+++ include/SGCLPDisplayPage.h
@@ -2,11 +2,17 @@
 #define SGCLPDISPLAYPAGE_H
 
 class SGWBackground;
+class SGWWidget;
+class SGXTimer;
 class SGCLPDisplayPage {
 public:
     static SGWBackground* instance;
     static SGWBackground* initialise();
     static void activate();
+    static int time;
+    static SGXTimer* timer;
+    static SGWWidget* progressBar;
+    static void updateProgressBar();
 };
 
 #endif // SGCLPDISPLAYPAGE_H
--- src/SGCLPDisplayPage.cpp
+++ src/SGCLPDisplayPage.cpp
@@ -2,13 +2,36 @@
 #include <SGWBackground.h>
 #include <SGWPageBackground.h>
 #include <SGWWidget.h>
+#include <SGXTimer.h>
+#include <SGWBlankWidget.h>
+#include <SGCLPOptionsPage.h>
 
 SGWBackground* SGCLPDisplayPage::instance = nullptr;
+int SGCLPDisplayPage::time = 0;
+SGXTimer* SGCLPDisplayPage::timer = nullptr;
+SGWWidget* SGCLPDisplayPage::progressBar = nullptr;
 
 SGWBackground* SGCLPDisplayPage::initialise(){
-    return new SGWPageBackground(SGWWidget::parentWidget, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 8);
+    SGWBackground* bg = new SGWPageBackground(SGWWidget::parentWidget, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 8);
+    SGCLPDisplayPage::time = 0;
+    SGCLPDisplayPage::progressBar = new SGWBlankWidget(bg, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 6);
+    SGCLPDisplayPage::timer = new SGXTimer(1.0f, &SGCLPDisplayPage::updateProgressBar);
+    return bg;
 }
 
 void SGCLPDisplayPage::activate(){
     SGWBackground::enable(SGCLPDisplayPage::instance, &SGCLPDisplayPage::initialise, nullptr);
+}
+
+void SGCLPDisplayPage::updateProgressBar(){
+    SGCLPDisplayPage::time++;
+    (*SGCLPDisplayPage::progressBar).setW1(static_cast<float>(SGCLPDisplayPage::time) / 10.0f);
+    if(SGCLPDisplayPage::time == 10){
+        SGCLPDisplayPage::time = 0;
+        (*SGCLPDisplayPage::timer).deleteTimer();
+        SGCLPDisplayPage::progressBar = nullptr;
+        SGCLPDisplayPage::timer = nullptr;
+        SGWBackground::disable(SGCLPDisplayPage::instance);
+        SGCLPOptionsPage::activate();
+    }
 }
